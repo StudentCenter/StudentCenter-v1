97f71041295e172f639e500850ace7fde7650a05
Muhammad Farras Jibran <farasjibran@gmail.com>
2021-01-30 00:11:58 +0700

    update 12.11

--- Screens/SettingScreen.js
+++ Screens/SettingScreen.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Appbar } from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
 
 const SettingScreen = () => {
@@ -8,11 +7,34 @@ const SettingScreen = () => {
             <View 
                 style={styles.container}
             >
+
+                {/* Box 1 */}
                 <View
                 style={styles.box}
                 >
-<Text>Test</Text>
+                    <Text>Test1</Text>
                 </View>
+                
+                <Text
+                    style={styles.textmore}
+                >
+                    More Setting
+                </Text>
+
+                {/* Box 2 */}
+                <View
+                style={styles.box2}
+                >
+                    <Text>Test2</Text>
+                </View>
+
+                {/* Text Version */}
+                <Text 
+                    style={styles.version}
+                >
+                        App V1.1 Beta
+                </Text>
+
             </View>
         </>
     )
@@ -21,7 +43,8 @@ const SettingScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column'
+        flexDirection: 'column',
+        justifyContent: 'center'
     },
     box: {
         backgroundColor: '#E2EEFF',
@@ -29,6 +52,26 @@ const styles = StyleSheet.create({
         width: '90%',
         marginLeft: '5%',
         borderRadius: 20
+    },
+    box2: {
+        backgroundColor: '#E2EEFF',
+        height: '40%',
+        width: '90%',
+        marginLeft: '5%',
+        borderRadius: 20,
+        marginTop: '5%'
+    },
+    version: {
+        textAlign: 'center',
+        marginTop: '5%',
+        fontWeight: 'bold'
+    },
+    textmore: {
+        fontSize: 17, 
+        fontWeight: 'bold',
+        color: '#40537E',
+        marginTop: '5%',
+        marginLeft: '5%'
     }
 })
 
