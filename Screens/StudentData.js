5c122c21e3675e133d9d3ef1e6e5c9fabc6658af
Muhammad Farras Jibran <farasjibran@gmail.com>
2021-02-04 17:04:06 +0700

    update 5.04

--- Screens/StudentData.js
+++ Screens/StudentData.js
@@ -1,16 +1,48 @@
 /* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import {View, StyleSheet, ScrollView} from 'react-native';
-import {Text, FAB, Card} from 'react-native-paper';
+import {Text, FAB, Card, useTheme} from 'react-native-paper';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const StudentData = () => {
+  const paperTheme = useTheme();
   return (
     <>
       <ScrollView>
+        {/* View Sort */}
+        <View style={styles.containersort}>
+          <MaterialCommunityIcons
+            name="text-subject"
+            color={paperTheme.colors.text}
+            size={30}
+          />
+          <Card
+            style={{
+              width: wp('20%'),
+              shadowColor: '#FFFFFF',
+              borderRadius: 15,
+              shadowOffset: {width: 0, height: 2},
+              shadowOpacity: 25,
+              shadowRadius: 15,
+              elevation: 5,
+            }}>
+            <Text
+              style={{
+                marginTop: wp('1%'),
+                marginLeft: wp('2.5%'),
+                fontWeight: 'bold',
+                fontSize: 15,
+              }}>
+              XII RPL 1
+            </Text>
+          </Card>
+        </View>
+
+        {/* View Data Siswa */}
         <View style={styles.container}>
           <Card style={styles.colums}>
             <Card style={styles.images} />
@@ -68,7 +100,7 @@ const styles = StyleSheet.create({
     marginBottom: wp('5'),
   },
   colums: {
-    marginTop: 10,
+    marginTop: hp('2%'),
     width: wp('90%'),
     height: hp('12%'),
     flexDirection: 'column',
@@ -83,9 +115,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#C4C4C4',
     width: wp('18%'),
     height: hp('9.5%'),
-    marginTop: wp('2'),
-    marginLeft: wp('2'),
+    marginTop: wp('2.5%'),
+    marginLeft: wp('2%'),
     borderRadius: 15,
+  },
+  containersort: {
+    flex: 1,
+    marginTop: wp('2'),
+    marginLeft: wp('5'),
+    flexDirection: 'row',
   },
 });
 
