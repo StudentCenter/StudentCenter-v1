d9ee22e55f476ab8785276e77ae0d2f555e46567
Muhammad Farras Jibran <50952131+farasjibran@users.noreply.github.com>
2021-02-14 12:22:16 +0700

    Update DetailSiswa.js

--- Screens/DetailSiswa.js
+++ Screens/DetailSiswa.js
@@ -180,7 +180,14 @@ const DetailSiswa = ({route}) => {
             </>
         )
     } else {
-      
+        return(
+            <ActivityIndicator 
+            animating={true} 
+            size={40}
+            color="#345EF0" 
+            style={styles.loading} 
+            />
+        )
     }
 }
 
